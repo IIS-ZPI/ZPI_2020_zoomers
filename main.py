9a51532dfb754184c62a7715af4b4d17e27c28cd
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2020-03-29 11:38:34 +0200

    Initial commit
    Added function to print personal info.

---
+++ main.py
@@ -0,0 +1,8 @@
+from utils import *
+
+def main():
+    printPersonalData("Adam", "kombajno678", "Operations")
+
+if __name__== "__main__":
+    main()
+    
---
+++ utils.py
@@ -0,0 +1,5 @@
+def printPersonalData(name, gitHubID, role):
+    print("Contributor:")
+    print("Name: " + name + "\nGitHubID: " +
+          gitHubID + "\nRole: " + role + "\n")
+    
