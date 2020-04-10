8a15447ada5b1378a5f85bfaab9e5a3d5492d4bd
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-04-10 11:53:06 +0200

    Added ProductCategoryEnum to distinguish category of products in our database related to provided table of sales tax rates in USA. Added class library for parsing Wikipedia's site, that delivers data about taxation. Added class that represents state of murica in abbreviated way.  Added extension method due to the fact that in project we aren't using .Net Core 2.0 or higher.

---
+++ zpi_aspnet_test/zpi_aspnet_test/Enumerators/ProductCategoryEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace zpi_aspnet_test.Enumerators
+{
+   public enum ProductCategoryEnum
+   {
+      Groceries, PreparedFood, PrescriptionDrug, NonPrescriptionDrug, Clothing, Intangibles
+   }
+}
---
+++ zpi_aspnet_test/zpi_aspnet_test/Extensions/KeyValuePairExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zpi_aspnet_test.Extensions
+{
+   public static class KeyValuePairExtensions
+   {
+      public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
+      {
+         key = tuple.Key;
+         value = tuple.Value;
+      }
+   }
+}
