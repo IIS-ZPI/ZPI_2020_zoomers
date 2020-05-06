1eb1e1f773e6202a3220d7591a77c90d1c285f6a
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-06 22:05:32 +0200

    Added Singleton class DatabaseConnector that connects to project's database using connection string, added PetaPoco package to perform database operations.

---
+++ zpi_aspnet_test/zpi_aspnet_test/DataBaseUtilities/DataBaseOperations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zpi_aspnet_test.DataBaseUtilities
+{
+   public class DatabaseOperations
+   {
+   }
+}
