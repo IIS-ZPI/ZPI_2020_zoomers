00bf498b913a1dc9ddf8c6ef09808ce17ef1d8c8
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-08 01:10:44 +0200

    Prepared all needed interfaces for database interaction layer

---
+++ zpi_aspnet_test/zpi_aspnet_test/DataBaseUtilities/Interfaces/ICategoryDatabaseAccess.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using zpi_aspnet_test.Models;
+
+namespace zpi_aspnet_test.DataBaseUtilities.Interfaces
+{
+	public interface ICategoryDatabaseAccess
+	{
+		ICollection<CategoryModel> GetCategories();
+		CategoryModel GetCategoryById(int id);
+		CategoryModel GetCategoryByName(string name);
+
+		void InsertCategory(string name);
+		void InsertCategory(CategoryModel category);
+
+		void UpdateCategory(CategoryModel category);
+		void UpdateCategory(int id, string value);
+
+		void DeleteCategory(CategoryModel model);
+		void DeleteCategory(int categoryId);
+		void DeleteCategory(string name);
+	}
+}
--- zpi_aspnet_test/zpi_aspnet_test/DataBaseUtilities/Interfaces/IProductDatabaseAccess.cs
+++ zpi_aspnet_test/zpi_aspnet_test/DataBaseUtilities/Interfaces/IProductDatabaseAccess.cs
@@ -9,5 +9,19 @@ namespace zpi_aspnet_test.DataBaseUtilities.Interfaces
       ProductModel GetProductById(int id);
       ICollection<ProductModel> GetProductsFromCategory(CategoryModel model);
       ProductModel GetProductByName(string name);
+
+      void InsertProduct(ProductModel product);
+      void InsertProduct(string name, CategoryModel category, double purchasePrice = 0.0, double preferredPrice = 0.0, double finalPrice = 0.0);
+      void InsertProduct(string name, string categoryName, double purchasePrice = 0.0, double preferredPrice = 0.0, double finalPrice = 0.0);
+
+      void UpdateProduct(ProductModel product);
+      void UpdateProduct(int productId, CategoryModel category = null, double purchasePrice = 0.0,
+	      double preferredPrice = 0.0, double finalPrice = 0.0, string productName = "");
+      void UpdateProduct(int productId, string categoryName = "", double purchasePrice = 0.0,
+	      double preferredPrice = 0.0, double finalPrice = 0.0, string productName = "");
+
+      void DeleteProduct(ProductModel product);
+      void DeleteProduct(int productId);
+      void DeleteProduct(string productName);
    }
 }
