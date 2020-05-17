0da3092e961c9db99d224b530ef042cbb7aa2627
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-17 19:48:26 +0200

    Repaired CategoryModel

--- zpi_aspnet_test/zpi_aspnet_test/Models/CategoryModel.cs
+++ zpi_aspnet_test/zpi_aspnet_test/Models/CategoryModel.cs
@@ -5,8 +5,9 @@ namespace zpi_aspnet_test.Models
    [TableName("Categories"), PrimaryKey("Id")]
    public class CategoryModel
    {
+      [Column]
       public int Id { get; set; }
-
+      [Column]
       public string Name { get; set; }
    }
 }
