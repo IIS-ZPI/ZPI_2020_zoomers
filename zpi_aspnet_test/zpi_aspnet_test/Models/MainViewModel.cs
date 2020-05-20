c0eb0f93198c56bb1f1cd53a5fada8edaa77d2a4
Luvkitri <janjedrzejewski@gmail.com>
2020-05-20 02:04:29 +0200

    New Model, made for home/main view.

---
+++ zpi_aspnet_test/zpi_aspnet_test/Models/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zpi_aspnet_test.Models
+{
+    public class MainViewModel
+    {
+        public SelectList ProductSelectList { get; set; }
+
+        public ProductModel ChosenProduct { get; set; }
+
+        public List<string> StateNameList { get; set; }
+
+        public double PurchasePrice { get; set; }
+
+        public double PreferredPrice { get; set; }
+
+        public List<double> Tax { get; set; }
+
+        public List<double> FinalPrice { get; set; }
+
+        public List<double> Margin { get; set; }
+
+        public bool ShowTable { get; set; }
+    }
+}
