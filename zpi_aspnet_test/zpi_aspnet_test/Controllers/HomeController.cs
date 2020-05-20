6f43f351ba452c3ae21da7ec5915de4d00625d28
Luvkitri <janjedrzejewski@gmail.com>
2020-05-20 02:01:35 +0200

    Added new controller handler, for filling the table.

--- zpi_aspnet_test/zpi_aspnet_test/Controllers/HomeController.cs
+++ zpi_aspnet_test/zpi_aspnet_test/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
+using zpi_aspnet_test.Algorithms;
 using zpi_aspnet_test.DataBaseUtilities;
 using zpi_aspnet_test.DataBaseUtilities.DAOs;
 using zpi_aspnet_test.Models;
@@ -17,10 +19,58 @@ namespace zpi_aspnet_test.Controllers
             DatabaseContextProvider.Instance.ConnectToDb("zoomers_sql_server");
             StandardProductDatabaseAccessor productDatabase = new StandardProductDatabaseAccessor();
 
-            ProductModel productModel = new ProductModel();
-            productModel.ProductList = new SelectList(productDatabase.GetProducts(), "id", "Name");
+            MainViewModel mainViewModel = new MainViewModel();
+            mainViewModel.ProductSelectList = new SelectList(productDatabase.GetProducts(), "Name", "Name");
+            mainViewModel.ShowTable = false;
+            return View(mainViewModel);
+        }
 
-            return View(productModel);
+        [HttpPost]
+        public ActionResult Index(string product, double preferredPriceInput)
+        {
+            DatabaseContextProvider.Instance.ConnectToDb("zoomers_sql_server");
+            StandardProductDatabaseAccessor productDatabase = new StandardProductDatabaseAccessor();
+            StandardStateDatabaseAccessor stateDatabase = new StandardStateDatabaseAccessor();
+
+            MainViewModel mainViewModel = new MainViewModel();
+            mainViewModel.ProductSelectList = new SelectList(productDatabase.GetProducts(), "Name", "Name");
+            List<StateOfAmericaModel> stateList = new List<StateOfAmericaModel>(stateDatabase.GetStates());
+
+            ProductModel chosenProduct = productDatabase.GetProductByName(product.Trim());
+            chosenProduct.PreferredPrice = preferredPriceInput;
+
+            mainViewModel.PurchasePrice = Math.Round(chosenProduct.PurchasePrice, 2);
+            mainViewModel.PreferredPrice = chosenProduct.PreferredPrice;
+
+            var stateNameList = new List<string>();
+            var finalPrice = new List<double>();
+            var tax = new List<double>();
+            var margin = new List<double>();
+
+            foreach (var state in stateList)
+            {
+                // State Name
+                stateNameList.Add(state.Name);
+
+                // Product final price in current state
+                Algorithm.CalculateFinalPrice(chosenProduct, state);
+                finalPrice.Add(chosenProduct.FinalPrice);
+
+                // Tax for current state
+                tax.Add(Algorithm.GetTax(chosenProduct, state)); 
+
+                // Margin for chosen product in current state
+                margin.Add(Algorithm.CalculateMargin(chosenProduct));
+            }
+
+            mainViewModel.Tax = tax;
+            mainViewModel.Margin = margin;
+            mainViewModel.FinalPrice = finalPrice;
+            mainViewModel.StateNameList = stateNameList;
+            mainViewModel.ChosenProduct = chosenProduct;
+            mainViewModel.ShowTable = true;
+
+            return View(mainViewModel);
         }
 
         public ActionResult About()
