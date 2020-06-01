f5e05ad2cabf093247e9e92e40a44a4391eadf04
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-01 23:11:04 +0200

    Added first test case and setup method.

--- zpi_aspnet_test/zpi_aspnet_test.Tests/AlgorithmTests.cs
+++ zpi_aspnet_test/zpi_aspnet_test.Tests/AlgorithmTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zpi_aspnet_test.Algorithms;
+using zpi_aspnet_test.Models;
 
 namespace zpi_aspnet_test.Tests
 {
@@ -11,59 +11,18 @@ namespace zpi_aspnet_test.Tests
 	[TestClass]
 	public class AlgorithmTests
 	{
-		public AlgorithmTests()
+		private StateOfAmericaModel _state;
+		private ProductModel _product;
+		[TestInitialize]
+		public void Setup()
 		{
-			//
-			// TODO: Add constructor logic here
-			//
+			_state = new StateOfAmericaModel();
 		}
-
-		private TestContext testContextInstance;
-
-		/// <summary>
-		///Gets or sets the test context which provides
-		///information about and functionality for the current test run.
-		///</summary>
-		public TestContext TestContext
-		{
-			get
-			{
-				return testContextInstance;
-			}
-			set
-			{
-				testContextInstance = value;
-			}
-		}
-
-		#region Additional test attributes
-		//
-		// You can use the following additional attributes as you write your tests:
-		//
-		// Use ClassInitialize to run code before running the first test in the class
-		// [ClassInitialize()]
-		// public static void MyClassInitialize(TestContext testContext) { }
-		//
-		// Use ClassCleanup to run code after all tests in a class have run
-		// [ClassCleanup()]
-		// public static void MyClassCleanup() { }
-		//
-		// Use TestInitialize to run code before running each test 
-		// [TestInitialize()]
-		// public void MyTestInitialize() { }
-		//
-		// Use TestCleanup to run code after each test has run
-		// [TestCleanup()]
-		// public void MyTestCleanup() { }
-		//
-		#endregion
 
 		[TestMethod]
-		public void TestMethod1()
+		public void CalculateFinalPriceShouldThrowNullReferenceExceptionIfProvidedProductIsNull()
 		{
-			//
-			// TODO: Add test logic here
-			//
+			Assert.ThrowsException<NullReferenceException>(() => Algorithm.CalculateFinalPrice(null, _state));
 		}
 	}
 }
