da7a447ca2ad301958ef68f446b81f46b8121c26
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-05 00:56:59 +0200

    Finished database dependency injection for controllers

--- zpi_aspnet_test/zpi_aspnet_test/Controllers/CategorySelectionController.cs
+++ zpi_aspnet_test/zpi_aspnet_test/Controllers/CategorySelectionController.cs
@@ -5,27 +5,34 @@ using System.Web;
 using System.Web.Mvc;
 using zpi_aspnet_test.DataBaseUtilities;
 using zpi_aspnet_test.DataBaseUtilities.DAOs;
+using zpi_aspnet_test.DataBaseUtilities.Interfaces;
 using zpi_aspnet_test.Models;
 
 namespace zpi_aspnet_test.Controllers
 {
     public class CategorySelectionController : Controller
     {
+	    private readonly ICategoryDatabaseAccess _categoryRepository;
+	    private readonly IStateDatabaseAccess _stateRepository;
+	    private readonly IProductDatabaseAccess _productRepository;
 
-        [HttpPost]
+	    public CategorySelectionController(ICategoryDatabaseAccess categoryRepository, IStateDatabaseAccess stateRepository, IProductDatabaseAccess productRepository)
+	    {
+		    _categoryRepository = categoryRepository;
+		    _stateRepository = stateRepository;
+		    _productRepository = productRepository;
+	    }
+
+	    [HttpPost]
         public ActionResult Index(string category)
         {
-            var productDatabase = new StandardProductDatabaseAccessor();
-            var categoryDatabase = new StandardCategoryDatabaseAccessor();
-            var stateDatabase = new StandardStateDatabaseAccessor();
-
             MainViewModel mainViewModel = new MainViewModel();
-            mainViewModel.ProductSelectList = new SelectList(productDatabase.GetProducts(), "Name", "Name");
-            mainViewModel.CategorySelectList = new SelectList(categoryDatabase.GetCategories(), "Name", "Name");
-            mainViewModel.StateSelectList = new SelectList(stateDatabase.GetStates(), "Name", "Name");
+            mainViewModel.ProductSelectList = new SelectList(_productRepository.GetProducts(), "Name", "Name");
+            mainViewModel.CategorySelectList = new SelectList(_categoryRepository.GetCategories(), "Name", "Name");
+            mainViewModel.StateSelectList = new SelectList(_stateRepository.GetStates(), "Name", "Name");
 
             mainViewModel.ProductList = new List<ProductModel>(
-                productDatabase.GetProductsFromCategory(categoryDatabase.GetCategoryByName(category)));
+                _productRepository.GetProductsFromCategory(_categoryRepository.GetCategoryByName(category)));
             mainViewModel.Category = category;
 
             return View(mainViewModel);
