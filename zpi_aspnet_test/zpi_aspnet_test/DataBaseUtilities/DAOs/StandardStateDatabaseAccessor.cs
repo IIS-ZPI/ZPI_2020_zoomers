5355d24ef989f9bef4996aff6e229e556c0efb11
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-04 23:28:45 +0200

    The last DAO update.

--- zpi_aspnet_test/zpi_aspnet_test/DataBaseUtilities/DAOs/StandardStateDatabaseAccessor.cs
+++ zpi_aspnet_test/zpi_aspnet_test/DataBaseUtilities/DAOs/StandardStateDatabaseAccessor.cs
@@ -19,31 +19,53 @@ namespace zpi_aspnet_test.DataBaseUtilities.DAOs
 
 		public ICollection<StateOfAmericaModel> GetStates()
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var states = _provider.DatabaseContext.Query<StateOfAmericaModel>("SELECT * FROM States").ToList();
+
+			_provider.DisconnectFromDb();
+
 			return states;
 		}
 
 		public StateOfAmericaModel GetStateById(int id)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
-			return db.FirstOrDefault<StateOfAmericaModel>("WHERE Id = @0", id);
+			var rV = db.FirstOrDefault<StateOfAmericaModel>("WHERE Id = @0", id);
+
+			_provider.DisconnectFromDb();
+
+			return rV;
 		}
 
 		public StateOfAmericaModel GetStateByName(string name)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
-			return db.FirstOrDefault<StateOfAmericaModel>("WHERE Name = @0", name);
+			var rV = db.FirstOrDefault<StateOfAmericaModel>("WHERE Name = @0", name);
+
+			_provider.DisconnectFromDb();
+
+			return rV;
 		}
 
 		public int InsertState(StateOfAmericaModel state)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if (db.FirstOrDefault<StateOfAmericaModel>("WHERE Name = @0", state.Name) != null) throw new ItemAlreadyExistsException();
 			db.Insert(state);
+
+			_provider.DisconnectFromDb();
+
 			return state.Id;
 		}
 
@@ -51,6 +73,8 @@ namespace zpi_aspnet_test.DataBaseUtilities.DAOs
 			double? prescriptionDrug = null, double? nonPrescriptionDrug = null, double? clothing = null,
 			double? intangibles = null)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if (db.FirstOrDefault<StateOfAmericaModel>("WHERE Name = @0", name) != null) throw new ItemAlreadyExistsException();
@@ -68,24 +92,34 @@ namespace zpi_aspnet_test.DataBaseUtilities.DAOs
 			};
 
 			db.Insert(state);
+
+			_provider.DisconnectFromDb();
+
 			return state.Id;
 		}
 
 		public void UpdateState(StateOfAmericaModel state)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if (db.FirstOrDefault<StateOfAmericaModel>("WHERE Id = @0", state.Id) == null) throw new ItemNotFoundException();
 			db.Update(state);
+
+			_provider.DisconnectFromDb();
 		}
 
 		public void UpdateState(int stateId, string name = "", double? baseSalesTax = null, double? groceries = null,
 			double? preparedFood = null, double? prescriptionDrug = null, double? nonPrescriptionDrug = null,
 			double? clothing = null, double? intangibles = null)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
-			if(!(db.FirstOrDefault<StateOfAmericaModel>("WHERE Id = @0", stateId) is StateOfAmericaModel model)) throw new ItemNotFoundException();
+			if(!(db.FirstOrDefault<StateOfAmericaModel>("WHERE Id = @0", stateId) is { } model)) throw new ItemNotFoundException();
+
 			model.Name = name;
 			model.Groceries = groceries ?? 0;
 			model.BaseSalesTax = baseSalesTax ?? 0;
@@ -94,31 +128,46 @@ namespace zpi_aspnet_test.DataBaseUtilities.DAOs
 			model.PrescriptionDrug = prescriptionDrug ?? 0;
 			model.Clothing = clothing ?? 0;
 			model.Intangibles = intangibles ?? 0;
+
 			db.Update(model);
+
+			_provider.DisconnectFromDb();
 		}
 
 		public void DeleteState(StateOfAmericaModel state)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if ((db.FirstOrDefault<StateOfAmericaModel>("WHERE Id = @0", state.Id) == null)) throw new ItemNotFoundException();
 			db.Delete(state);
+
+			_provider.DisconnectFromDb();
 		}
 
 		public void DeleteState(int id)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
-			if (!(db.FirstOrDefault<StateOfAmericaModel>("WHERE Id = @0", id) is StateOfAmericaModel model)) throw new ItemNotFoundException();
+			if (!(db.FirstOrDefault<StateOfAmericaModel>("WHERE Id = @0", id) is { } model)) throw new ItemNotFoundException();
 			db.Delete(model);
+
+			_provider.DisconnectFromDb();
 		}
 
 		public void DeleteState(string name)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
-			if (!(db.FirstOrDefault<StateOfAmericaModel>("WHERE Name = @0", name) is StateOfAmericaModel model)) throw new ItemNotFoundException();
+			if (!(db.FirstOrDefault<StateOfAmericaModel>("WHERE Name = @0", name) is { } model)) throw new ItemNotFoundException();
 			db.Delete(model);
+
+			_provider.DisconnectFromDb();
 		}
 
 		public void SetProvider(IDatabaseContextProvider provider)
