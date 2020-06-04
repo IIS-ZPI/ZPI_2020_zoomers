3d24c3ac3d182e563d648f8ecac204b9df6c461b
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-04 23:07:12 +0200

    Next step of DB improvements, added temporary connection to each operation on DB. We should do some tests to check performance of that solution.

--- zpi_aspnet_test/zpi_aspnet_test/DataBaseUtilities/DAOs/StandardCategoryDatabaseAccessor.cs
+++ zpi_aspnet_test/zpi_aspnet_test/DataBaseUtilities/DAOs/StandardCategoryDatabaseAccessor.cs
@@ -19,82 +19,129 @@ namespace zpi_aspnet_test.DataBaseUtilities.DAOs
 
 		public ICollection<CategoryModel> GetCategories()
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var categories = _provider.DatabaseContext.Query<CategoryModel>("SELECT * FROM Categories").ToList();
+
+			_provider.DisconnectFromDb();
+
 			return categories;
 		}
 
 		public CategoryModel GetCategoryById(int id)
 		{
-			if(!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
+			_provider.ConnectToDb();
+
+			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
-			return db.FirstOrDefault<CategoryModel>("WHERE Id = @0", id);
+			var rV = db.FirstOrDefault<CategoryModel>("WHERE Id = @0", id);
+
+			_provider.DisconnectFromDb();
+
+			return rV;
 		}
 
 		public CategoryModel GetCategoryByName(string name)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
-			return db.FirstOrDefault<CategoryModel>("WHERE Name = @0", name);
+			var rV = db.FirstOrDefault<CategoryModel>("WHERE Name = @0", name);
+
+			_provider.DisconnectFromDb();
+
+			return rV;
 		}
 
 		public int InsertCategory(string name)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if (db.FirstOrDefault<CategoryModel>("WHERE Name = @0", name) != null) throw new ItemAlreadyExistsException();
 			var category = new CategoryModel(){Name = name};
 			db.Insert(category);
+
+			_provider.DisconnectFromDb();
+
 			return category.Id;
 		}
 
 		public int InsertCategory(CategoryModel category)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if(db.FirstOrDefault<CategoryModel>("WHERE Name = @0", category.Name) != null) throw new ItemAlreadyExistsException();
 			db.Insert(category);
+
+			_provider.DisconnectFromDb();
+
 			return category.Id;
 		}
 
 		public void UpdateCategory(CategoryModel category)
 		{
-			if(!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
+			_provider.ConnectToDb();
+
+			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if(db.FirstOrDefault<CategoryModel>("WHERE Id = @0", category.Id) == null) throw new ItemNotFoundException();
 			db.Update(category);
+			
+			_provider.DisconnectFromDb();
 		}
 
 		public void UpdateCategory(int id, string value)
 		{
-			if(!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
+			_provider.ConnectToDb();
+
+			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if(db.FirstOrDefault<CategoryModel>("WHERE Id = @0", id) == null) throw new ItemNotFoundException();
 			db.Update<CategoryModel>("SET Name = @1 WHERE Id = @0", id, value);
+			
+			_provider.DisconnectFromDb();
 		}
 
 		public void DeleteCategory(CategoryModel model)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if(db.FirstOrDefault<CategoryModel>("WHERE Id = @0", model.Id) == null) throw new ItemNotFoundException();
 			db.Delete(model);
+
+			_provider.DisconnectFromDb();
 		}
 
 		public void DeleteCategory(int categoryId)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if(db.FirstOrDefault<CategoryModel>("WHERE Id = @0", categoryId) == null) throw new ItemNotFoundException();
 			db.Delete<CategoryModel>("WHERE Id = @0", categoryId);
+
+			_provider.DisconnectFromDb();
 		}
 
 		public void DeleteCategory(string name)
 		{
+			_provider.ConnectToDb();
+
 			if (!_provider.Connected) throw new AccessToNotConnectedDatabaseException();
 			var db = _provider.DatabaseContext;
 			if (db.FirstOrDefault<CategoryModel>("WHERE Name = @0", name) == null) throw new ItemNotFoundException();
 			db.Delete<CategoryModel>("WHERE Name = @0", name);
+
+			_provider.DisconnectFromDb();
 		}
 
 		public void SetProvider(IDatabaseContextProvider provider)
