086e1b2e9f6bd07b7231d5e769b4476c9a88bb14
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-20 13:51:56 +0200

    Added tax model

---
+++ zpi_aspnet_test/zpi_aspnet_test/Models/TaxModel.cs
@@ -0,0 +1,26 @@
+﻿using PetaPoco;
+
+namespace zpi_aspnet_test.Models
+{
+	[TableName("Taxes"), PrimaryKey("Id")]
+	public class TaxModel
+	{
+		[Column] 
+		public int Id { get; set; }
+
+		[Column] 
+		public int CategoryId { get; set; }
+
+		[Column] 
+		public int StateId { get; set; }
+
+		[Column]
+		public double MinValue { get; set; }
+
+		[Column]
+		public double MaxValue { get; set; }
+
+		[Column]
+		public double TaxRate { get; set; }
+	}
+}
