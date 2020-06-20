ef0ec918dc4f7844161a5c634b4f6c71a893f6b5
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-20 18:43:38 +0200

    Added TaxBuilder, adjusted StateBuilder and StateSelectionController

--- zpi_aspnet_test/zpi_aspnet_test.Tests/Builders/StateBuilder.cs
+++ zpi_aspnet_test/zpi_aspnet_test.Tests/Builders/StateBuilder.cs
@@ -1,44 +1,31 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
 using zpi_aspnet_test.Models;
 
 namespace zpi_aspnet_test.Tests.Builders
 {
 	public class StateBuilder : IBuilder<StateOfAmericaModel>
 	{
-		private double _groceries;
-		private double _clothing;
 		private string _name;
-		private double _intangibles;
 		private double _baseSalesTax;
-		private double _nonPrescriptionDrugs;
-		private double _prescriptionDrugs;
-		private double _preparedFood;
 		private int _id;
+
+		internal readonly List<TaxModel> Taxes = new List<TaxModel>();
 
 		public StateOfAmericaModel Build() => new StateOfAmericaModel
 		{
-			Clothing = _clothing,
 			Name = _name,
-			Groceries = _groceries,
-			Intangibles = _intangibles,
 			BaseSalesTax = _baseSalesTax,
-			NonPrescriptionDrug = _nonPrescriptionDrugs,
-			PreparedFood = _preparedFood,
-			PrescriptionDrug = _prescriptionDrugs,
+			TaxRates = Taxes,
 			Id = _id
 		};
 
 		public static StateBuilder State() => new StateBuilder();
 
-		public StateBuilder WithGroceriesTaxRate(double tax)
-		{
-			_groceries = tax;
-			return this;
-		}
+		public TaxBuilder AddTax() => new TaxBuilder(_id, this);
 
-		public StateBuilder WithClothingTaxRate(double tax)
+		public StateBuilder OfBaseSalesTax(double tax)
 		{
-			_clothing = tax;
+			_baseSalesTax = tax;
 			return this;
 		}
 
@@ -48,41 +35,10 @@ namespace zpi_aspnet_test.Tests.Builders
 			return this;
 		}
 
-		public StateBuilder WithIntangiblesTaxRate(double tax)
-		{
-			_intangibles = tax;
-			return this;
-		}
-
-		public StateBuilder WithBaseSalesTaxRate(double tax)
-		{
-			_baseSalesTax = tax;
-			return this;
-		}
-
-		public StateBuilder WithNonPrescriptionDrugTaxRate(double tax)
-		{
-			_nonPrescriptionDrugs = tax;
-			return this;
-		}
-
-		public StateBuilder WithPrescriptionDrugTaxRate(double tax)
-		{
-			_prescriptionDrugs = tax;
-			return this;
-		}
-
-		public StateBuilder WithPreparedFoodTaxRate(double tax)
-		{
-			_preparedFood = tax;
-			return this;
-		}
-
 		public StateBuilder OfId(int id)
 		{
 			_id = id;
 			return this;
 		}
-
 	}
 }
