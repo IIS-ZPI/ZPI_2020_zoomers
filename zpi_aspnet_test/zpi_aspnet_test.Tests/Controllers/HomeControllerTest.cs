30ad1d271064343b5cc3d5fcba45b36ab1dfa76c
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-21 12:07:47 +0200

    Added base of unit tests of DAO dependencies injected into controllers.

--- zpi_aspnet_test/zpi_aspnet_test.Tests/Controllers/HomeControllerTest.cs
+++ zpi_aspnet_test/zpi_aspnet_test.Tests/Controllers/HomeControllerTest.cs
@@ -4,65 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 using zpi_aspnet_test;
 using zpi_aspnet_test.Controllers;
-using zpi_aspnet_test.DataBaseUtilities.DAOs;
 using zpi_aspnet_test.DataBaseUtilities.Interfaces;
 
 namespace zpi_aspnet_test.Tests.Controllers
 {
-	 [TestClass]
-	 public class HomeControllerTest
-	 {
-		 private ICategoryDatabaseAccess _categoryRepository;
-		 private IStateDatabaseAccess _stateRepository;
-		 private IProductDatabaseAccess _productRepository;
+	[TestClass]
+	public class HomeControllerTest
+	{
+		private ICategoryDatabaseAccess _categoryRepository;
+		private IStateDatabaseAccess _stateRepository;
+		private IProductDatabaseAccess _productRepository;
 
-		 [TestInitialize]
-		 public void Setup()
-		 {
-			 _stateRepository = new StandardStateDatabaseAccessor();
-			 _productRepository = new StandardProductDatabaseAccessor();
-			 _categoryRepository = new StandardCategoryDatabaseAccessor();
-		 }
+		[TestInitialize]
+		public void Setup()
+		{
+			_stateRepository = Substitute.For<IStateDatabaseAccess>();
+			_productRepository = Substitute.For<IProductDatabaseAccess>();
+			_categoryRepository = Substitute.For<ICategoryDatabaseAccess>();
+		}
 
-		  [TestMethod]
-		  public void Index()
-		  {
-				// Arrange
-				HomeController controller = new HomeController(_categoryRepository, _stateRepository, _productRepository);
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			HomeController controller = new HomeController(_categoryRepository, _stateRepository, _productRepository);
 
-				// Act
-				ViewResult result = controller.Index() as ViewResult;
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
 
-				// Assert
-				Assert.IsNotNull(result);
-		  }
+			// Assert
+			Assert.IsNotNull(result);
+		}
 
-		  [TestMethod]
-		  public void About()
-		  {
-				// Arrange
-				HomeController controller = new HomeController(_categoryRepository, _stateRepository, _productRepository);
+		[TestMethod]
+		public void About()
+		{
+			// Arrange
+			HomeController controller = new HomeController(_categoryRepository, _stateRepository, _productRepository);
 
-				// Act
-				ViewResult result = controller.About() as ViewResult;
+			// Act
+			ViewResult result = controller.About() as ViewResult;
 
-				// Assert
-				Assert.AreEqual("About this app", result.ViewBag.Message);
-		  }
+			// Assert
+			Assert.AreEqual("About this app", result.ViewBag.Message);
+		}
 
-		  [TestMethod]
-		  public void Contact()
-		  {
-				// Arrange
-				HomeController controller = new HomeController(_categoryRepository, _stateRepository, _productRepository);
+		[TestMethod]
+		public void Contact()
+		{
+			// Arrange
+			HomeController controller = new HomeController(_categoryRepository, _stateRepository, _productRepository);
 
-				// Act
-				ViewResult result = controller.Contact() as ViewResult;
+			// Act
+			ViewResult result = controller.Contact() as ViewResult;
 
-				// Assert
-				Assert.IsNotNull(result);
-		  }
-	 }
+			// Assert
+			Assert.IsNotNull(result);
+		}
+	}
 }
