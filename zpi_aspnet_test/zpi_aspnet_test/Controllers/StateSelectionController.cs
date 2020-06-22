df60391888f13de07aa52bc49773bcdfee85d806
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-23 00:19:23 +0200

    Refactoring again StateSelectionController

--- zpi_aspnet_test/zpi_aspnet_test/Controllers/StateSelectionController.cs
+++ zpi_aspnet_test/zpi_aspnet_test/Controllers/StateSelectionController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Web;
 using System.Web.Mvc;
 using zpi_aspnet_test.Algorithms;
 using zpi_aspnet_test.DataBaseUtilities.Interfaces;
 using zpi_aspnet_test.ViewModels;
+using static zpi_aspnet_test.Utilities.DoubleUtilities;
 
 namespace zpi_aspnet_test.Controllers
 {
@@ -38,51 +38,29 @@ namespace zpi_aspnet_test.Controllers
 			var chosenState = _stateDatabase.GetStateByName(state.Trim());
 			var chosenProduct = _productDatabase.GetProductByName(product.Trim());
 
-
-			var format = new NumberFormatInfo();
-			if (preferredPriceInput.Contains(".") && preferredPriceInput.Contains(","))
-			{
-				format.NumberGroupSeparator = ",";
-				format.NumberDecimalSeparator = ".";
-			}
-			else if (preferredPriceInput.Contains("."))
-			{
-				format.NumberDecimalSeparator = ".";
-			}
-			else if (preferredPriceInput.Contains(","))
-			{
-				format.NumberDecimalSeparator = ",";
-			}
-
-
-			chosenProduct.PreferredPrice = Convert.ToDouble(preferredPriceInput, format);
-
-			var stateNameList = new List<string>();
-			var finalPrice = new List<double>();
-			var tax = new List<double>();
-			var margin = new List<double>();
+			chosenProduct.PreferredPrice = ParsePrice(preferredPriceInput);
 
 			// State Name
-			stateNameList.Add(chosenState.Name);
+			var stateNameList = new List<string> {chosenState.Name};
 
 			// Product final price in current state
 			Algorithm.CalculateFinalPrice(chosenProduct, chosenState, count);
-			finalPrice.Add(chosenProduct.FinalPrice);
+			var finalPriceList = new List<double> {chosenProduct.FinalPrice};
 
 			// Tax for current state
-			tax.Add(Algorithm.GetTax(chosenProduct, chosenState, count));
+			var taxes = new List<double> {Algorithm.GetTax(chosenProduct, chosenState, count)};
 
 			// Margin for chosen product in current state
-			margin.Add(Algorithm.CalculateMargin(chosenProduct, count));
+			var margins = new List<double> {Algorithm.CalculateMargin(chosenProduct, count)};
 
 			var mainViewModel = new MainViewModel
 			{
 				ProductSelectList = new SelectList(productModels, "Name", "Name"),
 				CategorySelectList = new SelectList(categoryModels, "Name", "Name"),
 				StateSelectList = new SelectList(stateOfAmericaModels, "Name", "Name"),
-				Tax = tax,
-				Margin = margin,
-				FinalPrice = finalPrice,
+				Tax = taxes,
+				Margin = margins,
+				FinalPrice = finalPriceList,
 				StateNameList = stateNameList,
 				ChosenProduct = chosenProduct,
 				PurchasePrice = Math.Round(chosenProduct.PurchasePrice, 2),
