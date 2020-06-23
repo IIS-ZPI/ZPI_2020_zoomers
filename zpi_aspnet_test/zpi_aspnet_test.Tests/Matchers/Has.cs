8d56ffb79ee72aa07531244091c18518754503cb
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-23 22:31:37 +0200

    Moved matchers to separated files, added CollectionMatcher

---
+++ zpi_aspnet_test/zpi_aspnet_test.Tests/Matchers/Has.cs
@@ -0,0 +1,30 @@
+﻿using NHamcrest;
+using NHamcrest.Core;
+
+namespace zpi_aspnet_test.Tests.Matchers
+{
+	public class Has<T> : Matcher<T>
+	{
+		private readonly IMatcher<T> _toDecorate;
+
+		public Has(IMatcher<T> toDecorate)
+		{
+			_toDecorate = toDecorate;
+		}
+
+		public override bool Matches(T item)
+		{
+			return _toDecorate.Matches(item);
+		}
+
+		public override void DescribeTo(IDescription description)
+		{
+			_toDecorate.DescribeTo(description);
+		}
+
+		public override void DescribeMismatch(T item, IDescription mismatchDescription)
+		{
+			_toDecorate.DescribeMismatch(item, mismatchDescription);
+		}
+	}
+}
---
+++ zpi_aspnet_test/zpi_aspnet_test.Tests/Matchers/Is.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NHamcrest;
+using NHamcrest.Core;
+
+namespace zpi_aspnet_test.Tests.Matchers
+{
+	public class Is<T> : Matcher<T>
+	{
+		private readonly IMatcher<T> _decoratedMatcher;
+
+		public Is(IMatcher<T> toDecorate)
+		{
+			_decoratedMatcher = toDecorate;
+		}
+
+		public static IMatcher<T> IsA(Type type)
+		{
+			return new Is<T>(new IsInstanceOf<T>());
+		}
+
+		public Is(T item)
+		{
+			_decoratedMatcher = Is.EqualTo(item);
+		}
+
+		public override bool Matches(T item)
+		{
+			return _decoratedMatcher.Matches(item);
+		}
+
+		public override void DescribeTo(IDescription description)
+		{
+			_decoratedMatcher.DescribeTo(description);
+		}
+
+		public override void DescribeMismatch(T item, IDescription mismatchDescription)
+		{
+			_decoratedMatcher.DescribeMismatch(item, mismatchDescription);
+		}
+	}
+}
