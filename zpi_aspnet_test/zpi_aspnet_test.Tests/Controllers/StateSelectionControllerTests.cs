14e1f95224e505b39711f70bbb6af98f8c779829
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-24 10:08:09 +0200

    Added setups

--- zpi_aspnet_test/zpi_aspnet_test.Tests/Controllers/StateSelectionControllerTests.cs
+++ zpi_aspnet_test/zpi_aspnet_test.Tests/Controllers/StateSelectionControllerTests.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using zpi_aspnet_test.DataBaseUtilities.Interfaces;
 
 namespace zpi_aspnet_test.Tests.Controllers
 {
-   [TestClass]
-   public class StateSelectionControllerTests
-   {
-	  [TestMethod]
-	  public void TestMethod1()
-	  {
-	  }
-   }
+	[TestClass]
+	public class StateSelectionControllerTests
+	{
+		private ICategoryDatabaseAccess _categoryRepository;
+		private IStateDatabaseAccess _stateRepository;
+		private IProductDatabaseAccess _productRepository;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_stateRepository = Substitute.For<IStateDatabaseAccess>();
+			_productRepository = Substitute.For<IProductDatabaseAccess>();
+			_categoryRepository = Substitute.For<ICategoryDatabaseAccess>();
+		}
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
 }
